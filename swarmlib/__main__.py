2e2f63277c1c75a4e94f93fe7a3e7bf2c017bcd4
Nikos Koutsovasilis <nikosk@admin.grnet.gr>
2020-11-16 19:37:47 +0200

    Use choices for the --log-level argument

--- swarmlib/__main__.py
+++ swarmlib/__main__.py
@@ -60,7 +60,8 @@ def run_swarm():
         help='Used to set the initial state of the random bit generator (default None)')
     parser.add_argument(
         '--log-level',
-        default='INFO',
+        default='info',
+        choices=['debug', 'info', 'warning', 'error', 'critical'],
         help='Set the log level')
 
     sub_parsers = parser.add_subparsers(
@@ -77,9 +78,8 @@ def run_swarm():
     if args:
         algorithm = args.pop('func')
         loglevel = args.pop('log_level', 'INFO')
-        level = getattr(logging, loglevel.upper(), None)
-        if level:
-            logging.getLogger().setLevel(level)
+        level = getattr(logging, loglevel.upper())
+        logging.getLogger().setLevel(level)
         algorithm(args)
     else:
         parser.print_usage()
